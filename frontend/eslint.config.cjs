7a48e609d52461aea77c90bd6ce6848c393888d2
GautamPince <ramgautam9128@gmail.com>
2026-09-05 13:32:06 +0530

    chore: update formatting in deploy-docs.yml and eslint.config.cjs

--- frontend/eslint.config.cjs
+++ frontend/eslint.config.cjs
@@ -1,15 +1,15 @@
 // ESLint config in CommonJS format expected by ESLint v9+ when using flat config
 module.exports = {
-	languageOptions: {
-		ecmaVersion: 2021,
-		sourceType: 'module'
-	},
-	linterOptions: {
-		reportUnusedDisableDirectives: true
-	},
-	rules: {
-		'no-unused-vars': ['warn'],
-		'react/prop-types': 'off'
-	},
-	ignores: ['dist/', 'node_modules/']
+   languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module'
+   },
+   linterOptions: {
+      reportUnusedDisableDirectives: true
+   },
+   rules: {
+      'no-unused-vars': ['warn'],
+      'react/prop-types': 'off'
+   },
+   ignores: ['dist/', 'node_modules/']
 };
